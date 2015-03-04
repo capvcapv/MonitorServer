f05d691e912c47c9f13959bda8d07556ae1294b8
Carlos Alberto Palacios Vargas <lord_capv@hotmail.com>
2015-03-03 21:15:30 -0600

    primer commit

---
+++ MonitorServidores/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace MonitorServidores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Iniciar monitoreo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 15000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Monitor de servidores";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 72);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Monitor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ MonitorServidores/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Configuration;
+
+namespace MonitorServidores
+{
+    public partial class Form1 : Form
+    {
+
+        private Ping checador;
+        private List<string> servidores;
+        private int contador;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checador = new Ping();
+            string ser = ConfigurationManager.AppSettings["servidores"];
+
+            servidores =new List<string>(ser.Split('|'));
+           
+            contador = 0;
+
+            timer1.Start();
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                timer1.Stop();
+                if (checador.Send(servidores[rota(contador)]).Status == IPStatus.Success)
+                {
+                    
+                    //notifyIcon1.BalloonTipTitle = "Servidor OK";
+                    //notifyIcon1.BalloonTipText =servidores[contador];
+                    //notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+                    //notifyIcon1.ShowBalloonTip(1000);
+                }
+                else
+                {
+                   
+                    notifyIcon1.BalloonTipTitle = "Servidor Error";
+                    notifyIcon1.BalloonTipText = servidores[contador];
+                    notifyIcon1.BalloonTipIcon = ToolTipIcon.Error;
+                    notifyIcon1.ShowBalloonTip(1000);
+                }
+                contador += 1;
+                timer1.Start();
+            }
+            catch (Exception oe)
+            {
+                
+                notifyIcon1.BalloonTipTitle = "Servidor Error";
+                notifyIcon1.BalloonTipText = servidores[contador];
+                notifyIcon1.BalloonTipIcon = ToolTipIcon.Error;
+                notifyIcon1.ShowBalloonTip(1000);
+                contador += 1;
+                timer1.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int rota(int i)
+        {
+            if (i == servidores.Count)
+            {
+                contador = 0;
+                return contador;
+            }
+            else
+            {
+                return contador;
+            }
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Visible = false;
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+   }
+}
